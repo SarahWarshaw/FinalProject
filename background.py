3ba52a8e9ecbebf06ce33538e52810d6a0d4ca72
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-12-10 15:15:28 +0000

    background and gf

---
+++ background.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+''' components: 
+- 2 motors (1 for rotating base, 1 for distributing card)
+- LED to show who's turn it is (user input of buttons)
+- photores to show if cards in holder or if empty
+- buttons (show if new round or if next turn)
+'''
+import RPi.GPIO as GPIO
+import time
+import json
+from stepper import Stepper
+import gamefunctions as gf
+import multiprocessing
+
+GPIO.setmode(GPIO.BCM)
+
+# Flags for game readiness
+readyToBegin = False
+
+#Button
+button = 12
+GPIO.setup(button, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+#multiprocessing for keypad
+noWinner = multiprocessing.value('i')
+noWinner = 1
+p = multiprocessing.Process(target = gf.check_keypad,args=(noWinner,))
+p.daemon = True
+p.start()
+
+while True:
+  try:
+      readyToBegin, game, num, playerInfo = gf.get_game_info()
+      if (readyToBegin):
+          print(playerInfo)
+          
+          gf.deal_setup(num, game)
+          currPlayer = 1
+          info_key = "Player"
+
+          if (game == "Go Fish!"):
+              print("Go Fish!")
+              noWinner = 1
+              info_key = info_key + str(currPlayer)
+              
+              gf.setup_turn(playerInfo[info_key][1])
+              
+              while (noWinner==1):
+                if GPIO.input(button) == 1:
+                  
+                  print("42")
+                  gf.deal_card()
+                  if (currPlayer < num):
+                      gf.end_turn(num, 1)
+                  # Arrange how to iterate by the dictionary keys
+                  # if we're already at last player, need to reverse motor
+                  
+                  oldPlayer = currPlayer
+                  
+                  if (currPlayer == num):
+                      currPlayer = 0
+                      gf.reverseMotor(num, -1)
+                      
+                  currPlayer += 1
+                  info_key = info_key.replace(str(oldPlayer), str(currPlayer))
+                  print(info_key)
+                      
+                  gf.setup_turn(playerInfo[info_key][1])
+                  # maybe for degrees have those set in an array or a tuple so that the degrees and direction are always the same
+                '''
+                if keypadPress==1:
+                  noWinner = False
+                  # Winner is corresponding to number pressed on keypad
+                  # display html screen with option of starting a new game
+          
+          elif (game == "Prez"):
+          noWinner = True
+          while (noWinner):
+            if buttonTurn == 1:
+              # change LED
+            if keypadPress ==1:
+              noWinner = False
+              # Winner displayed on html screen
+    ''' 
+              print(noWinner)
+  except Exception as e:
+    print(e)
+    #GPIO.cleanup()
+    
---
+++ gamefunctions.py
@@ -0,0 +1,178 @@
+'''
+Header file containing methods for card game functionality
+'''
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+import re
+from stepper import Stepper
+import json
+
+GPIO.setmode(GPIO.BCM)
+
+# Motor setup
+myStepper = Stepper()
+DCpin = 20 
+GPIO.setup(DCpin, GPIO.OUT)
+pwm = GPIO.PWM(DCpin,100)
+pwm.start(0)
+
+# Keypad setup
+rows = [26,19,13,6]
+cols = [5,27,17,4]
+
+
+#LED setup
+pins = (16, 24, 25) # R = 13, G = 24, B = 25
+GPIO.setup(16, GPIO.OUT, initial=1)
+GPIO.setup(24, GPIO.OUT, initial=1)
+GPIO.setup(25, GPIO.OUT, initial=1)
+
+pwmR = GPIO.PWM(16, 2000)  # set each PWM pin to 2 KHz
+pwmG = GPIO.PWM(24, 2000)
+pwmB = GPIO.PWM(25, 2000)
+pwmR.start(0)   # initially set to 0 duty cycle
+pwmG.start(0)
+pwmB.start(0)
+
+for i in range(4):
+  GPIO.setup(rows[i], GPIO.OUT)
+  GPIO.setup(cols[i], GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+def get_game_info():
+    # var to return
+    myPlayers = {}
+    ready = False
+    varCheck = False
+    
+    # Need to read in files with game and player information
+    # Check if files have already been read in
+    with open("/usr/lib/cgi-bin/home-screen.txt", 'r') as f:
+      data = json.load(f) 
+    # read from text doc
+    game = data["game_choice"]
+    num = int(data["player_num"])
+    
+    if (game != "Going"):
+        
+        # Next, open up the player information file
+        with open("/usr/lib/cgi-bin/player-screen.txt", 'r') as f:
+            playerData = json.load(f)
+        
+        if (playerData["Player1"] != "Going"):
+            
+            varCheck = True
+
+            for player in playerData.keys():
+                playerPattern = '^Name:(.*),,,Color:\((\d{1,3}),(\d{1,3}),(\d{1,3})\)$'
+                myMatch = re.search(playerPattern, playerData[player])
+                if myMatch:
+                    myColors = []
+                    for i in range(0,3):
+                        myColors.append(int(myMatch.group(i+2)))
+                    
+                    myPlayers[player] = [myMatch.group(1), myColors]
+                    
+            # Write to file that game is now going on
+            data = {"game_choice":"Going", "player_num":num}
+            with open("/usr/lib/cgi-bin/home-screen.txt", 'w') as f:
+                json.dump(data, f)
+            
+            # Write to file that game is now going on
+            data = {"Player1":"Going", "player_num":"Done"}
+            with open("/usr/lib/cgi-bin/player-screen.txt", 'w') as f:
+                json.dump(data, f)
+                
+            ready = True
+
+    # Do we have to overwrite the files?
+    return ready, game, num, myPlayers
+
+# based on game, deal the correct cards
+def deal_setup(numUsers, myGame):
+    
+    global myStepper
+    
+    playerAngle = 360/numUsers
+    
+    if (myGame == "Go Fish!"):
+        # https://bicyclecards.com/how-to-play/go-fish/
+        if (numUsers < 4):
+            multiplier = 7
+        else:
+            multiplier = 5
+    elif (myGame == "Prez"):
+        multiplier = 52/numUsers
+    
+    
+    myDir = 1
+    multiplier = 2
+    for i in range (0, multiplier):
+    
+        for j in range(0, numUsers):
+            # need to adjust duty cycle for time for one card
+            print(j)
+            deal_card()
+            if (j < numUsers - 1):
+                myStepper.goAngle(playerAngle,myDir)
+        
+        myStepper.goAngle(360 - playerAngle,-1)
+
+def setup_turn(colorList):
+    # display the LED color of the specific player
+    # Need to map from 0 - 255 to 0 - 100
+
+    r = int(100 * (colorList[0] / 255))
+    g = int(100 * (colorList[1] / 255))
+    b = int(100 * (colorList[2] / 255))
+
+    print(r,g,b)
+
+    pwmR.ChangeDutyCycle(r)
+    pwmG.ChangeDutyCycle(g)
+    pwmB.ChangeDutyCycle(b)
+    
+def end_turn(numUsers, myDir):
+
+    playerAngle = 360/numUsers
+    myStepper.goAngle(playerAngle, myDir)
+
+def reverseMotor(numUsers, myDir):
+    playerAngle = 360/numUsers
+    myStepper.goAngle(360 - playerAngle, myDir)
+
+def deal_card():
+    global pwm
+    pwm.ChangeDutyCycle(70)
+    sleep(5)
+    pwm.ChangeDutyCycle(0)
+ 
+def check_keypad(noWinner):
+  while True:
+    try:
+      noWinner = 1
+      Winner = "noWinner"
+      Winner = readLine(rows[0], ["1","2","3","A"],Winner,noWinner)
+      Winner = readLine(rows[1], ["4","5","6","B"],Winner,noWinner)
+      Winner = readLine(rows[2], ["7","8","9","C"],Winner,noWinner)
+      Winner = readLine(rows[3], ["*","0","#","D"],Winner,noWinner)
+    except Exception as e:
+      print(e)
+    
+    
+def readLine(line, characters,Winner,noWinner):
+  GPIO.output(line, GPIO.HIGH)
+  if(GPIO.input(cols[0]) == 1):
+    Winner = "player" +(characters[0])
+    noWinner = 0
+  if(GPIO.input(cols[1]) == 1):
+    Winner = "player" +(characters[1])
+    noWinner = 0
+  if(GPIO.input(cols[2]) == 1):
+    Winner = "player" +(characters[2])
+    noWinner = 0
+  if(GPIO.input(cols[3]) == 1):
+    Winner = "player" +(characters[3])
+    noWinner = 0
+  GPIO.output(line, GPIO.LOW)
+  return Winner
